7e65450b3ee589441b99ed9f5bcd92769c26acfd
olayode <aolayode@getcarbon.co>
2022-02-19 08:03:11 +0100

    project setup

---
+++ src/main/java/com/possible/mmkamal/MmKamalApplication.java
@@ -0,0 +1,13 @@
+package com.possible.mmkamal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmKamalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmKamalApplication.class, args);
+	}
+
+}
